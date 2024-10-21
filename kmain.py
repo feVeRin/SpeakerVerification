38af5c31693989434889bfafbcc7eca60cd3dcae
feVeRin <jwjln3126@naver.com>
2024-10-21 12:39:23 +0900

    Update kmain.py

--- kmain.py
+++ kmain.py
@@ -1,11 +1,20 @@
 import yaml
+import re
+import glob
+import torch
 
 from train import train
+from test import load_model, load_audio
+import backend.cosine_similarity_full as csf
 
 if __name__ == '__main__':
-    # parameter setting
-    print('Load yaml..')
+    # =====================================
+    # Parameter Setting
+    # =====================================
+    CKPT = True
+    ckpt_name = 'ckpt_1.pt' ####
     
+    print('Load yaml..')
     with open('K_NeXt_TDNN.yaml') as file:
         config = yaml.safe_load(file)
     
@@ -20,8 +29,39 @@ if __name__ == '__main__':
     DEVICE = config['PARAMS']['DEVICE']
     BASE_PATH = config['PARAMS']['BASE_PATH']
     
-    # training
-    train(config, MAX_EPOCH, BATCH_SIZE, NUM_WORKER, BASE_LR, BASE_PATH, DEVICE)
+    # =====================================
+    # Train
+    # =====================================
+    if CKPT:
+        train(config, MAX_EPOCH, BATCH_SIZE, NUM_WORKER, BASE_LR, BASE_PATH, DEVICE, ckpt=CKPT, ckpt_name=ckpt_name)
+    else:
+        train(config, MAX_EPOCH, BATCH_SIZE, NUM_WORKER, BASE_LR, BASE_PATH, DEVICE)
     
-    # test
+    # =====================================
+    # Test
+    # =====================================
+    regex = re.compile(r'(?<=\\)(.*?)(?=.wav)')
+    enroll_files = glob.glob('enrollment/*.wav') 
+    test_audio_file = 'KHOtest.wav' ####
     
+    model = load_model(config, ckpt_name, DEVICE)
+    model.eval()
+    
+    test_audio = load_audio(test_audio_file, DEVICE)
+    test_emb = model(test_audio.unsqueeze(0))
+    
+    # verification
+    score = {}
+    with torch.no_grad():
+        for enroll in enroll_files:
+            enr_audio = load_audio(enroll, DEVICE)
+            enr_emb = model(enr_audio.unsqueeze(0))
+            similarity = csf.cosine_similarity_full(enr_emb, test_emb)
+            score[regex.search(enroll).group(0)] = similarity
+        
+    verification = max(score, key=score.get) # speaker name with max similarity 
+    
+    if verification == test_audio_file[:3]:
+        print('!!!!! Verification Success (Speaker : {0}) !!!!!'.format(verification))
+    else:
+        print('!!!!! Verification Fail !!!!!')
